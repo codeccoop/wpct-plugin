c27dd109302f0520750107dd8a69932afe513ed7
Lucas García <lucas@codeccoop.org>
2025-11-04 01:02:54 +0100

    feat: add blueprint tests for addons and integrations

---
+++ tests/test-json-schema-utils.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Class JsonSchemaUtilsTest
+ *
+ * @package wpct-plugin-tests
+ */
+
+/**
+ * Json schema utils test case.
+ */
+class JsonSchemaUtilsTest extends WP_UnitTestCase {
+}
---
+++ tests/test-setting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * clas SettingTest
+ *
+ * @pacakge wpct-plugin-tests
+ */
+
+/**
+ * Setting object test case.
+ */
+class SettingTest extends WP_UnitTestCase {
+}
